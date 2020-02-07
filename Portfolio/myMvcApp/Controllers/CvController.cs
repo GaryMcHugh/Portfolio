30f11db48a2be3e841fec02b6155d3dcbe6f826b
STEPSTONE-ASA\mchugg01 <mchugg01@stepstone.com>
2020-02-07 11:03:31 +0000

    adding Portfolio website to github

---
+++ Portfolio/myMvcApp/Controllers/CvController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using myMvcApp.Models;
+
+namespace myMvcApp.Controllers
+{
+    public class CvController : Controller
+    {
+        // POST: /Cv/DownlaodCV
+        [HttpGet]
+        public FileResult DownloadCv()
+        {
+            return File(Server.MapPath("~/Content/myCV/GaryMcHugh_cv.pdf"), "application/pdf", "GaryMcHugh_cv.pdf");
+        }
+
+    }
+}
---
+++ Portfolio/myMvcApp/Controllers/EmailController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using myMvcApp.Models;
+
+namespace myMvcApp.Controllers
+{
+    public class EmailController : Controller
+    {
+        // POST: /Email/SendEmail
+        [HttpPost]
+        public string SendEmail(string name, string email, string phone, string message)
+        {
+            try
+            {
+                // Credentials
+                var credentials = new NetworkCredential("emailAddress", "password");
+                // Mail message
+                var mail = new MailMessage()
+                {
+                    From = new MailAddress("noreply@garymchugh.com"),
+                    Subject = "Website Contact Form",
+                    Body = "You have received a new message from your website contact form.<br />" +
+                           "Here are the details:<br />Name:" + name + "<br />Email: " + email + "<br />Phone:" + phone +
+                           "<br />Message:<br />" + message + "; "
+                };
+
+                mail.IsBodyHtml = true;
+                mail.To.Add(new MailAddress("emailAddress"));
+                // Smtp client
+                var client = new SmtpClient()
+                {
+                    Port = 587,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Host = "smtp.gmail.com",
+                    EnableSsl = true,
+                    Credentials = credentials
+                };
+                client.Send(mail);
+                return "Email Sent Successfully!";
+            }
+            catch (System.Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
